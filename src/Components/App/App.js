375ea79d16ad47ea64ecc064b009155b01a7dde6
tarasovnm <tarasovnm@gmail.com>
2021-01-21 17:07:52 +0400

    Initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import s from './App.module.scss';
+import Form from '../Form/Form';
+
+function App() {
+  return (
+    <div className={s.App}>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,9 @@
+import s from './Form.module.scss';
+
+function Form() {
+  return (
+    <div className={s.Form}>form component</div>
+  );
+}
+
+export default Form;
